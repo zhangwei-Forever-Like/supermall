b0d6c112d5f7d8811a89e69e851c408157e77720
zwzwzw2019 <1517388906@qq.com>
2021-04-21 13:01:23 +0800

    vue.js

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -9,27 +9,47 @@
       </div>
       <el-input id="ipt"></el-input>
       <el-input id="ipt"></el-input>
-      <el-button id="btn2">登录</el-button>
+      <el-button id="btn2" @click="loginSystem">登录</el-button>
     </el-card>
+     <li v-for='(item,index2) in this.topicsArr' :key=index2>{{item.title}}</li>
   </div>
 </template>
 
 <script>
-export default {};
+import Axios from "axios";
+export default {
+  data() {
+    return {
+      topicsArr: []
+    };
+  },
+  methods: {
+    loginSystem() {
+      Axios.get("/login").then(function (response) {
+        let data = response.data;
+        if (data.success) {
+          that.topicsArr = data.data;
+          console.log(that.topicsArr);
+        }
+        console.log(response);
+      });
+    },
+  },
+};
 </script>
 
 <style>
 #Login {
   width: 400px;
   height: 300px;
-  margin-left: 100px;
+  margin-left: 400px;
   padding-top: 60px;
 }
 #btn2 {
   margin-top: 10px;
   margin-left: 150px;
 }
-#ipt{
-    margin-top: 10px;
+#ipt {
+  margin-top: 10px;
 }
 </style>
